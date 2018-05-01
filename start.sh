c4bae79ae7d5428df54576df0d5daa09d0953135
Elliott Forde <evforde@outlook.com>
2018-05-01 05:05:45 +0000

    [start] Print to stdout

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ mkdir mnt
 . venv/bin/activate
 pip3 install -e .
 env PYTHONUNBUFFERED=1 venv/bin/secfs-server server.sock > server.log &
-sudo env PYTHONUNBUFFERED=1 venv/bin/secfs-fuse PYRO:secfs@./u:server.sock mnt/ root.pub user-0-key.pem "user-$(id -u)-key.pem" > client.log &
+sudo env PYTHONUNBUFFERED=1 venv/bin/secfs-fuse PYRO:secfs@./u:server.sock mnt/ root.pub user-0-key.pem "user-$(id -u)-key.pem" # > client.log &
